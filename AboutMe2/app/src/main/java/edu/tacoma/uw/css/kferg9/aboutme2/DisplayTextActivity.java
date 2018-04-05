6bce7a5195877f167f2b8acc8f1bc3eb975512e6
MochiCurry <kferg9@uw.edu>
2018-04-05 14:27:35 -0700

    test first
    Commit to master

---
+++ AboutMe2/app/src/main/java/edu/tacoma/uw/css/kferg9/aboutme2/DisplayTextActivity.java
@@ -0,0 +1,16 @@
+package edu.tacoma.uw.css.kferg9.aboutme2;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class DisplayTextActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_text2);
+    }
+}
