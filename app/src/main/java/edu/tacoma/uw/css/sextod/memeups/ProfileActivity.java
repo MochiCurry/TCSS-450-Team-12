197cb8ab0d340ebf47899fd1c25747958c618e73
No-Mouse <35830636+No-Mouse@users.noreply.github.com>
2018-05-06 20:21:29 -0700

    Added profile and link to meme collection
    Most framework for the app is done, still need to add sign in, save data, and populate the quiz, matches, and meme collection.

--- app/src/main/java/edu/tacoma/uw/css/sextod/memeups/ProfileActivity.java
+++ app/src/main/java/edu/tacoma/uw/css/sextod/memeups/ProfileActivity.java
@@ -1,13 +1,32 @@
 package edu.tacoma.uw.css.sextod.memeups;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    private Button collectionbutton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        collectionbutton = findViewById(R.id.collectionbutton);
+        collectionbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openCollectionPage();
+            }
+        });
+    }
+
+    public void openCollectionPage() {
+        Intent intent = new Intent(this, CollectionActivity.class);
+        startActivity(intent);
+
     }
 }
