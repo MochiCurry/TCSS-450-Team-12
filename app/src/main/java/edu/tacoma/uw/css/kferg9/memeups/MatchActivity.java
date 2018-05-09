e47c9d5f42488026c08fba8a641b2bedbcfd96cf
MochiCurry <kferg9@uw.edu>
2018-05-09 13:04:11 -0700

    added actual quiz and database webserver kinda

---
+++ app/src/main/java/edu/tacoma/uw/css/kferg9/memeups/MatchActivity.java
@@ -0,0 +1,13 @@
+package edu.tacoma.uw.css.kferg9.memeups;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MatchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_match);
+    }
+}
