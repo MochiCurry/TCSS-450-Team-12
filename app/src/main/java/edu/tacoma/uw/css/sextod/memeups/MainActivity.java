72e3ee90524e201624f4616c3ebd342668965d62
fwtravisbain <fwtravisbain@outlook.com>
2018-05-11 08:17:38 -0700

    added register
    issues:
    -adding new users doesn't work properly
    -bringing up registration fragment doesn't get rid of login screen

--- app/src/main/java/edu/tacoma/uw/css/sextod/memeups/MainActivity.java
+++ app/src/main/java/edu/tacoma/uw/css/sextod/memeups/MainActivity.java
@@ -1,6 +1,7 @@
 package edu.tacoma.uw.css.sextod.memeups;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -10,9 +11,20 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity implements RegisterFragment.RegisterListener {
     private Button signinbutton;
+    private Button newuserbutton;
 
 
     @Override
@@ -33,7 +45,99 @@ public class MainActivity extends AppCompatActivity {
                 openMainPage();
             }
         });
+
+        newuserbutton = (Button) findViewById(R.id.newuserbutton);
+        newuserbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                RegisterFragment registerFragment = new RegisterFragment();
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container, registerFragment)
+                        .addToBackStack(null)
+                        .commit();
+            }
+        });
+
     }
+
+    private class AddUserTask extends AsyncTask<String, Void, String> {
+
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(String... urls) {
+            String response = "";
+            HttpURLConnection urlConnection = null;
+            for (String url : urls) {
+                try {
+                    URL urlObject = new URL(url);
+                    urlConnection = (HttpURLConnection) urlObject.openConnection();
+
+                    InputStream content = urlConnection.getInputStream();
+
+                    BufferedReader buffer = new BufferedReader(new InputStreamReader(content));
+                    String s = "";
+                    while ((s = buffer.readLine()) != null) {
+                        response += s;
+                    }
+
+                } catch (Exception e) {
+                    response = "Unable to add course, Reason: "
+                            + e.getMessage();
+                } finally {
+                    if (urlConnection != null)
+                        urlConnection.disconnect();
+                }
+            }
+            return response;
+        }
+
+
+        /**
+         * It checks to see if there was a problem with the URL(Network) which is when an
+         * exception is caught. It tries to call the parse Method and checks to see if it was successful.
+         * If not, it displays the exception.
+         *
+         * @param result
+         */
+        @Override
+        protected void onPostExecute(String result) {
+            // Something wrong with the network or the URL.
+            try {
+                JSONObject jsonObject = new JSONObject(result);
+                String status = (String) jsonObject.get("result");
+                if (status.equals("success")) {
+                    Toast.makeText(getApplicationContext(), "User successfully added!"
+                            , Toast.LENGTH_LONG)
+                            .show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Failed to add: "
+                                    + jsonObject.get("error")
+                            , Toast.LENGTH_LONG)
+                            .show();
+                }
+            } catch (JSONException e) {
+                Toast.makeText(getApplicationContext(), "Something wrong with the data" +
+                        e.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+
+    @Override
+    public void register(String url) {
+
+        AddUserTask task = new AddUserTask();
+        task.execute(new String[]{url.toString()});
+
+// Takes you back to the previous fragment by popping the current fragment out.
+        getSupportFragmentManager().popBackStackImmediate();
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
