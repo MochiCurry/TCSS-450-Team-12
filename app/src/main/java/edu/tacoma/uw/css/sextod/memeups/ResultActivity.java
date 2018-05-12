d72c55aea4a97d4b90d32ae7e166ee94e0face46
MochiCurry <kferg9@uw.edu>
2018-05-11 21:47:47 -0700

    final fix to return button

--- app/src/main/java/edu/tacoma/uw/css/sextod/memeups/ResultActivity.java
+++ app/src/main/java/edu/tacoma/uw/css/sextod/memeups/ResultActivity.java
@@ -1,6 +1,7 @@
 package edu.tacoma.uw.css.sextod.memeups;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -8,9 +9,12 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class ResultActivity extends AppCompatActivity {
+
+    private Button returnButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,8 +37,16 @@ public class ResultActivity extends AppCompatActivity {
         editor.putInt("totalScore", totalScore);
         editor.commit();
 
+        returnButton = findViewById(R.id.returnButton);
+        returnButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+                Intent intent = new Intent(getApplicationContext(), MainPage.class);
+                startActivity(intent);
 
+            }
+        });
 
     }
 
