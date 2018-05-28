c8586f87bb9b8330535f2d47f998f79ee0755d48
DirkScs <dirkasourus@gmail.com>
2018-05-28 12:56:16 -0700

    Added share content, and partial image

---
+++ main/java/edu/tacoma/uw/css/sextod/memeups/RegisterFragment.java
@@ -0,0 +1,236 @@
+/**
+ * Register fragment opens the page for the user to create an account using an email address and password.
+ * The email address used must be unique and valid; the password must be at least 5 characters long.
+ * These fields are put into a url string that is used to pass the email and password into the php
+ * file hosted on the server to add this new user into the database.
+ *
+ * @author Travis Bain
+ * @author Kerry Fergurson
+ * @author Dirk Sexton
+ * @version 1.0
+ * @since 1.0
+ */
+
+package edu.tacoma.uw.css.sextod.memeups;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.net.URLEncoder;
+
+import static android.content.ContentValues.TAG;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link RegisterFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link RegisterFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class RegisterFragment extends Fragment {
+
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+    private static final String ARG_PARAM3 = "param3";
+    private static final String ARG_PARAM4 = "param4";
+    private static final String ARG_PARAM5 = "param5";
+    private final static String COURSE_ADD_URL
+            = "http://kferg9.000webhostapp.com/android/addUser.php?";
+
+
+    private RegisterListener mListener;
+
+    private Button registerbutton;
+
+    private String mParam1;
+    private String mParam2;
+    private String mParam3;
+    private String mParam4;
+    private String mParam5;
+    private EditText mEmail;
+    private EditText mPassword;
+    private EditText mFirst;
+    private EditText mLast;
+    private EditText mUsername;
+
+    /**
+     * Listener for the registration button
+     */
+    public interface RegisterListener {
+        public void register(String url);
+    }
+
+    /**
+     * Empty constructor
+     */
+    public RegisterFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment RegisterFragment.
+     */
+    public static RegisterFragment newInstance(String param1, String param2, String param3, String param4, String param5) {
+        RegisterFragment fragment = new RegisterFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        args.putString(ARG_PARAM3, param3);
+        args.putString(ARG_PARAM4, param4);
+        args.putString(ARG_PARAM5, param5);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    /**
+     * Initialize the variables on create
+     * @param savedInstanceState
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+            mParam3 = getArguments().getString(ARG_PARAM3);
+            mParam4 = getArguments().getString(ARG_PARAM4);
+            mParam5 = getArguments().getString(ARG_PARAM5);
+        }
+
+    }
+
+    /**
+     * Initializes the variables to the values of the text fields and listens for the register button
+     * @param inflater
+     * @param container
+     * @param savedInstanceState
+     * @return
+     */
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_register, container, false);
+
+        //Set email and password
+        mEmail = (EditText) v.findViewById(R.id.registerEmail);
+        mPassword = (EditText) v.findViewById(R.id.registerPassword);
+        mFirst = (EditText) v.findViewById(R.id.registerFirst);
+        mLast = (EditText) v.findViewById(R.id.registerLast);
+        mUsername = (EditText) v.findViewById(R.id.registerUsername);
+
+        //Button for registration
+        Button button = (Button)
+                getActivity().findViewById(R.id.newuserbutton);
+        Button registerButton = (Button) v.findViewById(R.id.registerbutton);
+        /**
+         * Listener for registration button
+         */
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Build url using email and password and then call register function with the url
+                String url = buildCourseURL(v);
+                mListener.register(url);
+            }
+        });
+
+        return v;
+    }
+
+    /**
+     * On attach function with listener
+     * @param context
+     */
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof RegisterListener) {
+            mListener = (RegisterListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement RegisterListener");
+        }
+    }
+
+    /**
+     * Detach sets listener to null
+     */
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        void onFragmentInteraction(Uri uri);
+    }
+
+    /**
+     * Builds a url to be passed into php registeration function using email and password
+     * @param v view
+     * @return The url in a string format
+     */
+    private String buildCourseURL(View v) {
+
+        StringBuilder sb = new StringBuilder(COURSE_ADD_URL);
+
+        try {
+            String email = mEmail.getText().toString();
+            sb.append("email=");
+            sb.append(URLEncoder.encode(email, "UTF-8"));
+            String password = mPassword.getText().toString();
+            sb.append("&password=");
+            sb.append(URLEncoder.encode(password, "UTF-8"));
+            String first = mFirst.getText().toString();
+            sb.append("&first=");
+            sb.append(URLEncoder.encode(first, "UTF-8"));
+            String last = mLast.getText().toString();
+            sb.append("&last=");
+            sb.append(URLEncoder.encode(last, "UTF-8"));
+            String username = mUsername.getText().toString();
+            sb.append("&username=");
+            sb.append(URLEncoder.encode(username, "UTF-8"));
+
+
+            //Log.i(TAG sb.toString());
+            Log.i(TAG, sb.toString());
+
+        }
+        catch(Exception e) {
+            Toast.makeText(v.getContext(), "Something wrong with the url" + e.getMessage(), Toast.LENGTH_LONG)
+                    .show();
+        }
+        return sb.toString();
+    }
+
+
+
+}
---
+++ main/java/edu/tacoma/uw/css/sextod/memeups/ResultActivity.java
@@ -0,0 +1,247 @@
+/**
+ * ResultActivity displays the results sent from the quiz. There is a button to take you back to the
+ * home screen.
+ *
+ * @author Travis Bain
+ * @author Kerry Fergurson
+ * @author Dirk Sexton
+ * @version 1.0
+ * @since 1.0
+ */
+
+package edu.tacoma.uw.css.sextod.memeups;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+
+import static android.content.ContentValues.TAG;
+
+/**
+ * Class to display the results of the quiz
+ * author: Kerry Ferguson
+ */
+public class ResultActivity extends AppCompatActivity {
+
+    private final static String RESULT_ADD_URL
+            = "http://kferg9.000webhostapp.com/android/updateUser.php?cmd=quizresult";
+
+    private Button returnButton;
+    private Button shareButton;
+    private String level0 = "Scrub";
+    private String level1 = "Squire";
+    private String level2 = "Alright";
+    private String level3 = "Spicy";
+    private String level4 = "Hokage";
+
+
+    /**
+     * On create, read in the quiz score and display them
+     * @param savedInstanceState
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+
+        // text for results
+        TextView resultLabel = (TextView) findViewById(R.id.resultLabel);
+        final TextView totalScoreLabel = (TextView) findViewById(R.id.totalScoreLabel);
+
+        // get the user score
+        // int score = getIntent().getIntExtra("Right_Answer_Count", 0);
+        int category = getIntent().getIntExtra("Right_Answer_Count", 0);
+
+        SharedPreferences settings = getSharedPreferences("memeups", Context.MODE_PRIVATE);
+        int totalScore = settings.getInt("totalScore", 0);
+        // totalScore+=score;
+
+        resultLabel.setText("Your meme level is");
+
+        if(category == 0) {
+            totalScoreLabel.setText(level0);
+        }
+        else if(category == 1) {
+            totalScoreLabel.setText(level1);
+        }
+        else if(category == 2) {
+            totalScoreLabel.setText(level2);
+        }
+        else if(category == 3) {
+            totalScoreLabel.setText(level3);
+        }
+        else if(category == 4) {
+            totalScoreLabel.setText(level4);
+        }
+        //resultLabel.setText(score + " / 7");
+        //totalScoreLabel.setText("Total Score: " + score);
+
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putInt("totalScore", totalScore);
+        editor.commit();
+
+        //build string
+        StringBuilder sb = new StringBuilder(RESULT_ADD_URL);
+
+        try {
+            SharedPreferences mLoginEmail = getSharedPreferences(getString(R.string.LOGIN_PREFS)
+                    , Context.MODE_PRIVATE);
+
+            String email = mLoginEmail.getString("email", "");
+            Log.i(TAG, "email should be here");
+            Log.i(TAG, email);
+
+
+            sb.append("&email=");
+            sb.append(URLEncoder.encode(email, "UTF-8"));
+            sb.append("&score=");
+            sb.append(category);
+
+
+            //Log.i(TAG sb.toString());
+            Log.i(TAG, sb.toString());
+
+        }
+        catch(Exception e) {
+//            Toast.makeText(view.getContext(), "Something wrong with the url" + e.getMessage(), Toast.LENGTH_LONG)
+//                    .show();
+        }
+
+        //should have string now, try updating result
+        AddUserTask task = new AddUserTask();
+        task.execute(new String[]{sb.toString()});
+
+
+
+        returnButton = findViewById(R.id.returnButton);
+        returnButton.setOnClickListener(new View.OnClickListener() {
+            /**
+             * Listener for the return button. Sends the user back to the main page.
+             * @param v
+             */
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(getApplicationContext(), HomeScreenActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+        shareButton = findViewById(R.id.shareButton);
+        shareButton.setOnClickListener(new View.OnClickListener() {
+            /**
+             * Listener for the return button. Sends the user back to the main page.
+             * @param v
+             */
+            @Override
+            public void onClick(View v) {
+
+                Intent share = new Intent(Intent.ACTION_SEND);
+                share.setType("text/plain");
+                share.putExtra(Intent.EXTRA_TEXT, "My meme level is " + totalScoreLabel.getText() + " on MemeUps!");
+
+                startActivity(Intent.createChooser(share, "Choose where you want to share"));
+
+            }
+        });
+
+    }
+
+    /**
+     * Private class to handle asynchronous loading of data
+     */
+    private class AddUserTask extends AsyncTask<String, Void, String> {
+        /**
+         * PreExecute
+         */
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        /**
+         * This function reads and returns the response from the webpage.
+         * @param urls url of the webpage
+         * @return Returns the output from the webpage
+         */
+        @Override
+        protected String doInBackground(String... urls) {
+            String response = "";
+            HttpURLConnection urlConnection = null;
+            for (String url : urls) {
+                try {
+                    URL urlObject = new URL(url);
+                    urlConnection = (HttpURLConnection) urlObject.openConnection();
+
+                    InputStream content = urlConnection.getInputStream();
+
+                    BufferedReader buffer = new BufferedReader(new InputStreamReader(content));
+                    String s = "";
+                    while ((s = buffer.readLine()) != null) {
+                        response += s;
+                    }
+
+                } catch (Exception e) {
+                    response = "Unable to add course, Reason: "
+                            + e.getMessage();
+                } finally {
+                    if (urlConnection != null)
+                        urlConnection.disconnect();
+                }
+            }
+            return response;
+        }
+
+
+        /**
+         * It checks to see if there was a problem with the URL(Network) which is when an
+         * exception is caught. It tries to call the parse Method and checks to see if it was successful.
+         * If not, it displays the exception.
+         *
+         * @param result
+         */
+//        @Override
+//        protected void onPostExecute(String result) {
+//            // Something wrong with the network or the URL.
+//            try {
+//                JSONObject jsonObject = new JSONObject(result);
+//                String status = (String) jsonObject.get("result");
+//                if (status.equals("success")) {
+//                    Toast.makeText(getApplicationContext(), "Success!"
+//                            , Toast.LENGTH_LONG)
+//                            .show();
+//                    //On successful login, go to main page
+//                    //openMainPage();
+//                } else {
+//                    Toast.makeText(getApplicationContext(), "Error: "
+//                                    + jsonObject.get("error")
+//                            , Toast.LENGTH_LONG)
+//                            .show();
+//                }
+//            } catch (JSONException e) {
+//                Toast.makeText(getApplicationContext(), "Something wrong with the data" +
+//                        e.getMessage(), Toast.LENGTH_LONG).show();
+//            }
+//        }
+    }
+
+}
